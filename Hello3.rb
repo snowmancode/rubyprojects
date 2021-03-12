0e3b90fec2e5b67658d963b0ab93a45019d7bad6
ShowCaseCode <showcasecode@outlook.com>
2021-03-12 00:51:08 -0500

    6th commit for March 12, 2021

--- Hello3.rb
+++ Hello3.rb
@@ -9,6 +9,15 @@ puts("The Multi line method is awesome in Ruby")
 puts("End of comment")
 
 
+def check_stock()
+    stock = 10
+    if stock < 1
+    puts "Sorry we are out of stock!"
+    end
+end
+
+check_stock()
+
 
 
 
---
+++ Hello4.rb
@@ -0,0 +1,8 @@
+def check_stock()
+    stock = 10
+    if stock < 1
+    puts "Sorry we are out of stock!"
+    end
+end
+
+check_stock()
