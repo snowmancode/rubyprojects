aa8531969c51122c48c12520ac7dfc5f58136b80
ShowCaseCode <showcasecode@outlook.com>
2021-03-12 00:39:46 -0500

    5th commit for March 12, 2021

--- Hello3.rb
+++ Hello3.rb
@@ -1,3 +1,14 @@
 puts("New File")
 puts("Shadow")
 puts("Drink Slay Love")
+puts("The Multi line method is awesome in Ruby")
+
+#Testing multi line comment
+#Testing some more
+
+puts("End of comment")
+
+
+
+
+
