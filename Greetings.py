409e700a05b281267a0d7e560ca697a782e9a965
ShowCaseCode <showcasecode@outlook.com>
2021-03-13 00:16:09 -0500

    3rd commit for March 13, 2021

--- Greetings.py
+++ Greetings.py
@@ -1,2 +1,3 @@
 print("Greetings from up above")
 print("Greetings accepted")
+print("Python will be used as my starter langauge for DevOps projects")
--- Greetings.rb
+++ Greetings.rb
@@ -1,2 +1,3 @@
 puts("Loving Ruby")
 puts("Ruby is such a clean language")
+puts("Ruby is one of my favorite engineering langauges")
